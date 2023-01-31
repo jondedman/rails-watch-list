4712872da73dc09a9a90fb210569b65b1b50acf2
Jon Dedman <jonathandedman@googlemail.com>
2023-01-31 21:19:34 +0000

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists do
+    resources :bookmarks
+  end
 
-  resources :movies
+  # resources :movies do
+  #   resources :bookmarks
+  # end
 end
+
+# notice The shallow: true option is used to optimize the URL structure by only including the parent resource ID in the URL for the nested resource's "new" and "edit" actions. For all other actions, the parent resource ID is included in the URL as a parameter.
+# resources :lists do
+#   resources :bookmarks, shallow: true
+# end
+
+# resources :movies do
+#   resources :bookmarks, shallow: true
+# end
+
+# GET "lists/42/bookmarks/new"
+# POST "lists/42/bookmarks"
